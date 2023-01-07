16d2e8f33d31058913abe604cd96f6a7e1cd1734
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-07 20:13:23 +0100

    removed old code

--- update.php
+++ update.php
@@ -10,10 +10,3 @@
 $addon = rex_addon::get('feeds');
 
 $addon->includeFile(__DIR__ . '/install.php');
-
-// if (rex_string::versionCompare($this->getVersion(), '1.1.0-beta3', '<')) {
-//     rex_sql_table::get(rex::getTable('feeds_item'))
-//         ->ensureColumn(new rex_sql_column('status', 'tinyint(1)', false, 1))
-//         ->alter();
-// }
-
