eb9c97696fbc5e00446bb1ff9b2bb5c2d828bbea
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-11 17:42:51 +0100

    refactoring to FeedIo

--- lib/stream/rss.php
+++ lib/stream/rss.php
@@ -32,36 +32,28 @@ class rex_feeds_stream_rss extends rex_feeds_stream_abstract
 
     public function fetch()
     {
-        $reader = new Reader();
-        $resource = $reader->download($this->typeParams['url'], $this->lastModified, $this->etag);
-        if (!$resource->isModified()) {
-            return;
-        }
-        $parser = $reader->getParser(
-            $resource->getUrl(),
-            $resource->getContent(),
-            $resource->getEncoding()
-        );
-        $feed = $parser->execute();
+        $client = new \FeedIo\Adapter\Guzzle\Client(new GuzzleHttp\Client());
+        $logger = new \Psr\Log\NullLogger();
+        $feedIo = new \FeedIo\FeedIo($client, $logger);
+
+        $result = $feedIo->read($this->typeParams['url']);
+
 
         /** @var Item $rssItem */
-        foreach ($feed->getItems() as $rssItem) {
-            $item = new rex_feeds_item($this->streamId, $rssItem->getId());
-            $item->setTitle($rssItem->getTitle());
+        foreach ($result->getFeed()  as $rssItem) {
+            $item = new rex_feeds_item($this->streamId, $rssItem->getPublicId());
+            $item->setTitle($item->getTitle());
             $item->setContentRaw($rssItem->getContent());
             $item->setContent(strip_tags($rssItem->getContent()));
-
-            $item->setUrl($rssItem->getUrl());
-            $item->setDate($rssItem->getDate());
-            $item->setAuthor($rssItem->getAuthor());
-            $item->setLanguage($rssItem->getLanguage());
-            if ($rssItem->getEnclosureUrl()) {
-                $item->setMedia($rssItem->getEnclosureUrl());
-            } elseif ($rssItem->getTag('media:content', 'url')) {
-                $item->setMedia($rssItem->getTag('media:content', 'url')[0]);
+            $item->setUrl($rssItem->getLink());
+            $item->setDate($rssItem->getLastModified());
+            // muss noch ermittelt werden. 
+            #$item->setAuthor($rssItem->getAuthor());
+            #$item->setLanguage($rssItem->getLanguage());
+            if ($rssItem->getMedias() && $rssItem->getMedias()[0]!='')
+            {
+                $item->setMedia($rssItem->getMedias()[0]->getUrl());
             }
-            $item->setRaw($rssItem);
-            
             $this->updateCount($item);
             $item->save();
         }
