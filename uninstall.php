60385fa7a639fad329dbd64e4bbf6e3124cd9bb1
Thomas Skerbis <thomas.skerbis@mac.com>
2023-01-15 23:14:44 +0100

    delete foreign key and tables

--- uninstall.php
+++ uninstall.php
@@ -1,4 +1,10 @@
 <?php 
+
+// delete ForeignKey
+rex_sql_table::get(rex::getTable('feeds_stream'))
+->removeForeignKey('rex_feeds_item_ibfk_1')
+->ensure();  
+
 # delete media effect
 $sql = rex_sql::factory();
 $sql->setTable(rex::getTablePrefix().'media_manager_type');
@@ -8,3 +14,7 @@ $sql->delete();
 $sql->setTable(rex::getTablePrefix().'media_manager_type_effect');
 $sql->setWhere(['createuser'=>'feeds']);
 $sql->delete();
+
+// delete tables
+$sql->setQuery(sprintf('DROP TABLE IF EXISTS `%s`;', rex::getTable('feeds_item')));
+$sql->setQuery(sprintf('DROP TABLE IF EXISTS `%s`;', rex::getTable('feeds_stream')));
