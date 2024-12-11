ea0bb4e145879eb47c3913854af6ad09e0534a8b
Thomas Skerbis <thomas.skerbis@mac.com>
2024-12-11 16:13:18 +0100

    null safe fix (#211)
    * null safe fix
    
    * Update package.yml

--- lib/stream/rss.php
+++ lib/stream/rss.php
@@ -40,24 +40,61 @@ class rex_feeds_stream_rss extends rex_feeds_stream_abstract
             echo rex_view::error($e->getMessage());
             return;
         }
+
         /** @var Item $rssItem */
-        foreach ($result->getFeed()  as $rssItem) {
-            $item = new rex_feeds_item($this->streamId, $rssItem->getPublicId());
-            $item->setTitle($rssItem->getTitle());
-            $item->setContentRaw($rssItem->getContent());
-            $item->setContent(strip_tags($rssItem->getContent()));
-            $item->setUrl($rssItem->getLink());
-            if($rssItem->getLastModified())
-            {
-            $item->setDate($rssItem->getLastModified());
+        foreach ($result->getFeed() as $rssItem) {
+            try {
+                // Sichere Extraktion der PublicId
+                $publicId = $rssItem->getPublicId();
+                if (empty($publicId)) {
+                    // Generiere eine eindeutige ID wenn keine vorhanden
+                    $publicId = uniqid('feed_', true);
+                }
+                
+                $item = new rex_feeds_item($this->streamId, $publicId);
+
+                // Titel mit Fallback
+                $title = $rssItem->getTitle();
+                $item->setTitle($title ?: '');
+
+                // Content mit Fallback
+                $content = $rssItem->getContent();
+                $item->setContentRaw($content ?: '');
+                $item->setContent($content ? strip_tags($content) : '');
+
+                // URL/Link mit Fallback
+                $link = $rssItem->getLink();
+                $item->setUrl($link ?: '');
+
+                // Datum mit Null-Check
+                $lastModified = $rssItem->getLastModified();
+                if ($lastModified) {
+                    $item->setDate($lastModified);
+                }
+
+                // Author mit Null-Check
+                $author = $rssItem->getAuthor();
+                $authorName = ($author && method_exists($author, 'getName')) ? $author->getName() : '';
+                $item->setAuthor($authorName);
+
+                // Media mit mehrfacher Null-Check
+                $medias = $rssItem->getMedias();
+                if ($medias && !empty($medias) && isset($medias[0])) {
+                    $mediaUrl = $medias[0]->getUrl();
+                    if ($mediaUrl) {
+                        $item->setMedia($mediaUrl);
+                    }
+                }
+
+                $this->updateCount($item);
+                $item->save();
+            } catch (Exception $e) {
+                // Logge Fehler für einzelne Items, aber fahre mit dem nächsten fort
+                rex_logger::logException($e);
+                continue;
             }
-            $item->setAuthor($rssItem->getAuthor()->getName());
-            if ($rssItem->getMedias() && isset($rssItem->getMedias()[0])) {
-                $item->setMedia($rssItem->getMedias()[0]->getUrl());
-            }
-            $this->updateCount($item);
-            $item->save();
         }
+
         self::registerExtensionPoint($this->streamId);
     }
 }
