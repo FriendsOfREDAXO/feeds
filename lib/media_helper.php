9812907974ca6328081b4991e7f00a5565287713
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-15 19:41:00 +0100

    Update media_helper.php

--- lib/media_helper.php
+++ lib/media_helper.php
@@ -33,14 +33,17 @@ class rex_feeds_media_helper
             // Create directory if it doesn't exist
             $mediaPath = self::getMediaPath();
             if (!is_dir($mediaPath)) {
-                mkdir($mediaPath, 0777, true);
+                rex_dir::create($mediaPath, true);
             }
 
             // Download and save file
             $response = rex_socket::factoryUrl($url)->doGet();
-            if ($response->isSuccess()) {
+            if ($response->isOk()) {
                 $filepath = $mediaPath . '/' . $filename;
-                if (file_put_contents($filepath, $response->getBody())) {
+                
+                // Use rex_file for safe file operations
+                if (rex_file::put($filepath, $response->getBody())) {
+                    @chmod($filepath, rex::getFilePerm());
                     return $filename;
                 }
             }
@@ -54,7 +57,7 @@ class rex_feeds_media_helper
     /**
      * Gets the absolute path to the media directory
      * 
-     * @return string
+     * @return string Absolute path to media directory
      */
     public static function getMediaPath()
     {
@@ -62,24 +65,10 @@ class rex_feeds_media_helper
     }
 
     /**
-     * Gets the web-accessible URL for a media file
-     * 
-     * @param string $filename The filename
-     * @return string|null
-     */
-    public static function getMediaUrl($filename)
-    {
-        if (!$filename) {
-            return null;
-        }
-        return rex_url::addonData('feeds', 'media/' . $filename);
-    }
-
-    /**
      * Deletes a media file
      * 
      * @param string $filename The filename to delete
-     * @return bool
+     * @return bool TRUE if successful, FALSE otherwise
      */
     public static function deleteMediaFile($filename)
     {
@@ -88,9 +77,77 @@ class rex_feeds_media_helper
         }
         
         $filepath = self::getMediaPath() . '/' . $filename;
-        if (file_exists($filepath)) {
-            return unlink($filepath);
+        return rex_file::delete($filepath);
+    }
+
+    /**
+     * Delete all media files for a specific stream
+     * 
+     * @param int $streamId The stream ID
+     * @return bool TRUE if successful, FALSE otherwise
+     */
+    public static function deleteStreamMedia($streamId)
+    {
+        if (!$streamId) {
+            return false;
         }
-        return false;
+
+        $mediaPath = self::getMediaPath();
+        if (!is_dir($mediaPath)) {
+            return true;
+        }
+
+        $pattern = $mediaPath . '/' . $streamId . '_*';
+        $files = glob($pattern);
+        
+        if (!$files) {
+            return true;
+        }
+
+        $success = true;
+        foreach ($files as $file) {
+            if (!rex_file::delete($file)) {
+                $success = false;
+            }
+        }
+
+        return $success;
+    }
+
+    /**
+     * Clean up orphaned media files that are no longer referenced in the database
+     * 
+     * @return int Number of deleted files
+     */
+    public static function cleanupOrphanedMedia()
+    {
+        $mediaPath = self::getMediaPath();
+        if (!is_dir($mediaPath)) {
+            return 0;
+        }
+
+        $files = glob($mediaPath . '/*');
+        if (!$files) {
+            return 0;
+        }
+
+        $deletedCount = 0;
+        $sql = rex_sql::factory();
+
+        foreach ($files as $file) {
+            $filename = basename($file);
+            
+            // Check if file is referenced in database
+            $query = 'SELECT id FROM ' . rex_feeds_item::table() . ' WHERE media_filename = ?';
+            $sql->setQuery($query, [$filename]);
+            
+            if (0 === $sql->getRows()) {
+                if (rex_file::delete($file)) {
+                    $deletedCount++;
+                }
+            }
+        }
+
+        return $deletedCount;
     }
 }
