57f2380a5409e269f6fea9c228c925d43517164a
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-15 21:07:32 +0100

    Update media_manager_effect.php

--- lib/media_manager_effect.php
+++ lib/media_manager_effect.php
@@ -1,14 +1,4 @@
 <?php
-
-/**
- * This file is part of the Feeds package.
- *
- * @author FriendsOfREDAXO
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 class rex_effect_feeds extends rex_effect_abstract
 {
     public function execute()
@@ -39,57 +29,9 @@ class rex_effect_feeds extends rex_effect_abstract
             return;
         }
 
-        // Get the physical file path
-        $filepath = rex_path::addonAssets('feeds', 'media/' . $mediaFilename);
-        if (!file_exists($filepath)) {
-            return;
-        }
-
-        // Get file extension
-        $extension = strtolower(pathinfo($mediaFilename, PATHINFO_EXTENSION));
-        
-        // Create image resource based on file type
-        $img = null;
-        switch ($extension) {
-            case 'jpg':
-            case 'jpeg':
-                $img = @imagecreatefromjpeg($filepath);
-                break;
-            case 'png':
-                $img = @imagecreatefrompng($filepath);
-                break;
-            case 'gif':
-                $img = @imagecreatefromgif($filepath);
-                break;
-            case 'webp':
-                if (function_exists('imagecreatefromwebp')) {
-                    $img = @imagecreatefromwebp($filepath);
-                }
-                break;
-            case 'avif':
-                if (function_exists('imagecreatefromavif')) {
-                    $img = @imagecreatefromavif($filepath);
-                }
-                break;
-        }
-
-        if (!$img) {
-            return;
-        }
-
-        // Preserve transparency for PNG and WebP
-        if (in_array($extension, ['png', 'webp'])) {
-            imagealphablending($img, true);
-            imagesavealpha($img, true);
-        }
-
-        $media = $this->media;
-        $media->setMediaPath(null);
-        $media->setMediaFilename($filename);
-        $media->setImage($img);
-        $media->setFormat($extension);
-        $media->setHeader('Content-Type', 'image/'.$extension);
-        $media->refreshImageDimensions();
+        // Set the media path to the feeds media file
+        $mediaPath = rex_path::addonData('feeds', 'media/' . $mediaFilename);
+        $this->media->setMediaPath($mediaPath);
     }
 
     public function getName()
