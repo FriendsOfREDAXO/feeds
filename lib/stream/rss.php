87eb791335245162ccb03669649e0a1ce18039be
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-16 10:21:52 +0100

    Update rss.php

--- lib/stream/rss.php
+++ lib/stream/rss.php
@@ -25,7 +25,13 @@ class rex_feeds_stream_rss extends rex_feeds_stream_abstract
         $feedIo = new \FeedIo\FeedIo($client, $logger);
 
         try {
+            rex_logger::logError(E_NOTICE, 'Starting feed fetch', ['url' => $this->typeParams['url']]);
             $result = $feedIo->read($this->typeParams['url']);
+            
+            $feed = $result->getFeed();
+            rex_logger::logError(E_NOTICE, 'Feed fetched', [
+                'itemCount' => iterator_count($feed)
+            ]);
 
             /** @var Item $rssItem */
             foreach ($result->getFeed() as $rssItem) {
@@ -35,6 +41,10 @@ class rex_feeds_stream_rss extends rex_feeds_stream_abstract
                     if (empty($publicId)) {
                         $publicId = uniqid('feed_', true);
                     }
+                    
+                    rex_logger::logError(E_NOTICE, 'Processing feed item', [
+                        'publicId' => $publicId
+                    ]);
                     
                     $item = new rex_feeds_item($this->streamId, $publicId);
 
@@ -47,6 +57,13 @@ class rex_feeds_stream_rss extends rex_feeds_stream_abstract
                     
                     // Versuche alle möglichen Content-Quellen
                     $elements = iterator_to_array($rssItem->getAllElements());
+                    
+                    rex_logger::logError(E_NOTICE, 'Found elements', [
+                        'count' => count($elements),
+                        'names' => array_map(function($el) { 
+                            return $el->getName(); 
+                        }, $elements)
+                    ]);
                     
                     foreach ($elements as $element) {
                         if ($element->getName() === 'content:encoded') {
@@ -86,42 +103,73 @@ class rex_feeds_stream_rss extends rex_feeds_stream_abstract
                     $authorName = ($author && method_exists($author, 'getName')) ? $author->getName() : '';
                     $item->setAuthor($authorName);
 
-                    // Media-Handling mit speziellem Mastodon Support
+                    // Media-Handling
                     $mediaUrl = null;
 
-                    // 1. Prüfe media:content für Mastodon
-                    foreach ($elements as $element) {
-                        if ($element->getName() === 'media:content') {
+                    // Hole alle MediaItems
+                    $mediaItems = $rssItem->getMedias();
+                    rex_logger::logError(E_NOTICE, 'Found media items', [
+                        'count' => count($mediaItems)
+                    ]);
+
+                    // Versuche zuerst über die MediaItems
+                    foreach ($mediaItems as $mediaItem) {
+                        rex_logger::logError(E_NOTICE, 'Checking media item', [
+                            'type' => $mediaItem->getType(),
+                            'url' => $mediaItem->getUrl()
+                        ]);
+                        
+                        if (strpos($mediaItem->getType(), 'image/') === 0) {
+                            $mediaUrl = $mediaItem->getUrl();
+                            rex_logger::logError(E_NOTICE, 'Found image in media items', [
+                                'url' => $mediaUrl
+                            ]);
+                            break;
+                        }
+                    }
+
+                    // Wenn kein Bild gefunden, versuche es über die Elemente
+                    if (!$mediaUrl) {
+                        foreach ($elements as $element) {
+                            $name = $element->getName();
                             $attributes = $element->getAttributes();
-                            if (isset($attributes['url']) && isset($attributes['type'])) {
-                                // Prüfe ob es ein Bild ist (type startet mit image/)
-                                if (strpos($attributes['type'], 'image/') === 0 && $attributes['url']) {
+                            
+                            rex_logger::logError(E_NOTICE, 'Checking element for media', [
+                                'name' => $name,
+                                'attributes' => $attributes
+                            ]);
+                            
+                            // Prüfe media:content
+                            if ($name === 'media:content') {
+                                if (isset($attributes['url']) && isset($attributes['medium']) && $attributes['medium'] === 'image') {
                                     $mediaUrl = $attributes['url'];
+                                    rex_logger::logError(E_NOTICE, 'Found image in media:content', [
+                                        'url' => $mediaUrl
+                                    ]);
+                                    break;
+                                }
+                            }
+                            
+                            // Prüfe enclosure
+                            if ($name === 'enclosure') {
+                                if (isset($attributes['url']) && isset($attributes['type']) && strpos($attributes['type'], 'image/') === 0) {
+                                    $mediaUrl = $attributes['url'];
+                                    rex_logger::logError(E_NOTICE, 'Found image in enclosure', [
+                                        'url' => $mediaUrl
+                                    ]);
                                     break;
                                 }
                             }
                         }
                     }
 
-                    // 2. Wenn kein media:content Bild gefunden, prüfe enclosure
-                    if (!$mediaUrl) {
-                        foreach ($elements as $element) {
-                            if ($element->getName() === 'enclosure') {
-                                $attributes = $element->getAttributes();
-                                if (isset($attributes['url']) && isset($attributes['type'])) {
-                                    if (strpos($attributes['type'], 'image/') === 0) {
-                                        $mediaUrl = $attributes['url'];
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                    // 3. Suche nach img-Tags in description/content
+                    // Als letztes im Content suchen
                     if (!$mediaUrl && $content) {
                         if (preg_match('/<img[^>]+src=[\'"]([^\'"]+)[\'"][^>]*>/i', $content, $matches)) {
                             $mediaUrl = $matches[1];
+                            rex_logger::logError(E_NOTICE, 'Found image in content', [
+                                'url' => $mediaUrl
+                            ]);
                         }
                     }
 
@@ -129,6 +177,14 @@ class rex_feeds_stream_rss extends rex_feeds_stream_abstract
                     if ($mediaUrl) {
                         $item->setMedia($mediaUrl);
                         $item->setMediaSource($mediaUrl);
+                        rex_logger::logError(E_NOTICE, 'Set media for item', [
+                            'url' => $mediaUrl,
+                            'publicId' => $publicId
+                        ]);
+                    } else {
+                        rex_logger::logError(E_NOTICE, 'No media found for item', [
+                            'publicId' => $publicId
+                        ]);
                     }
 
                     $this->updateCount($item);
